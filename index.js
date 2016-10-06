117e5a8a4fa0195b4714a57c40245221e6e4cb90
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2016-10-06 16:30:40 -0700

    From the toe, to the floor.

---
+++ index.js
@@ -0,0 +1,30 @@
+var BB = require('bluebird');
+
+function recreateElementsScriptTags(elementInQuestion) {
+    // http://stackoverflow.com/questions/13390588/script-tag-create-with-innerhtml-of-a-div-doesnt-work
+    return BB.all([].map.call(elementInQuestion.querySelectorAll('script'), function(scriptTag) {
+        var newScriptElement = document.createElement('script');
+
+        newScriptElement.type = 'text/javascript';
+
+        if(!scriptTag.src) {
+            newScriptElement.appendChild(document.createTextNode(scriptTag.innerHTML));
+
+            scriptTag.parentElement.insertBefore(newScriptElement, scriptTag);
+            scriptTag.parentElement.removeChild(scriptTag);
+
+            return true;
+        } else {
+            newScriptElement.src = scriptTag.src;
+            newScriptElement.async = scriptTag.async;
+
+            return new BB(function(resolve, reject) {
+                newScriptElement.onload = resolve;
+                newScriptElement.onerror = reject;
+
+                scriptTag.parentElement.insertBefore(newScriptElement, scriptTag);
+                scriptTag.parentElement.removeChild(scriptTag);
+            });
+        }
+    }));
+}
